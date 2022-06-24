3ca835c80484acd4d28530048fd31823ab21a5d0
Petar Juric <petarjuricbepo@yahoo.com>
2022-06-23 20:49:45 -0700

    drawable points on image. Working on scaling long+lat coordinates to pixels

--- flight.py
+++ flight.py
@@ -4,10 +4,18 @@ import requests
 import json
 from PIL import Image, ImageDraw
 
+###COORDINATES###
+topleft_lat = 90
+topleft_long = 180
+botright_lat = -90
+botright_long = 0
+
+
 r = requests.get('http://api.open-notify.org/iss-now.json')
 
 if (r.status_code > 200):
     print("status code: " + r.status_code)
+    exit()
 else:
     print("connection successful")
 
@@ -23,11 +31,20 @@ for data in rawData['iss_position']['latitude']:
 print("longitude: " + longitude)
 print("latitude: " + latitude)
 
-with Image.open("../FlightPath/map.jpg") as im:
+
+
+
+
+with Image.open("map.png") as im:
+
+    image_width, image_height = im.size # this is where things get interesting
+
+    #offsetting x,y grid to fit long + lat coordinates from iss position to fit image
+    #x_offset = (float(longitude) - topleft_long) / (botright_long - topleft_long) * image_width #too bad this doesnt work yet
+    #x_offset = (float(latitude) - topleft_lat) / (botright_lat - topleft_lat) * image_height
+
 
     draw = ImageDraw.Draw(im)
-    draw.line((0, 0) + im.size, fill=128)
-    draw.line((0, im.size[1], im.size[0], 0), fill=128)
-
-    # write to stdout
-    im.save(sys.stdout, "PNG") #making initial cross on map.jpg.. testing PIL
+    draw.point((1000,100) + im.size, fill="red")
+    im.save("map_point.png") #making initial cross on map.jpg.. testing PIL
+    im.show()
