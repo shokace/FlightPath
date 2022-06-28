7b6bb055de2669d40d8f6e1a8b6e937fef9b16f8
Petar Juric <petarjuricbepo@yahoo.com>
2022-06-28 03:31:06 -0700

    last push for a little while until i figure out how to update the image in realtime. from my understanding so far, i should use either opencv or matplotlib for that task..

--- flight.py
+++ flight.py
@@ -1,54 +1,42 @@
+import time
 import requests
 import json
-from PIL import Image, ImageDraw
-
-r = requests.get('http://api.open-notify.org/iss-now.json')
-
-if (r.status_code > 200):
-    print("status code: " + r.status_code)
-    exit()
-else:
-    print("connection successful")
-
-longitude = ""
-latitude = ""
+from PIL import Image
 
 
-rawData = json.loads(r.text)
-for data in rawData['iss_position']['longitude']:
-    longitude += data
-for data in rawData['iss_position']['latitude']:
-    latitude += data
+def run():
+    r = requests.get('http://api.open-notify.org/iss-now.json')
 
-print("longitude: " + longitude)
-print("latitude: " + latitude)
+    if (r.status_code > 200):
+        print("status code: " + r.status_code)
+        exit()
+    else:
+        print("connection successful")
 
+    longitude = ""
+    latitude = ""
 
-##BACKGROUND IMAGE INITIALIZATION##
+    rawData = json.loads(r.text)
+    for data in rawData['iss_position']['longitude']:
+        longitude += data
+    for data in rawData['iss_position']['latitude']:
+        latitude += data
 
-bg = Image.open("map.png")
-image_width, image_height = bg.size
+    print("longitude: " + longitude)
+    print("latitude: " + latitude)
 
+    ##BACKGROUND IMAGE INITIALIZATION##
+    bg = Image.open("map.png")
+    image_width, image_height = bg.size
 
+    x = int((image_width / 360) * (180 + float(longitude)))
+    y = int((image_height / 180) * (90 - float(latitude)))
 
-x = int((image_width/360) * (180 + float(longitude)))
-y = int((image_height/180) * (90 - float(latitude)))
+    ##ISS ICON INITIALIZATION##
+    icon = Image.open("iss.png")
+    bg.paste(icon, (x, y), mask=icon.im)
 
-
-##ISS ICON INITIALIZATION## 3444228.8365710177
-
-icon = Image.open("iss.png")
-bg.paste(icon, (x,y), mask = icon.im)
-
-
-
-
-
-
-bg.show()
-
-
-
+    bg.show()
 
 
-
+run()
