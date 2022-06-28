ddc272a48372a8cc525587a8b69eb25f719e3af0
Petar Juric <petarjuricbepo@yahoo.com>
2022-06-28 02:01:12 -0700

    cleaning unnecessary code.

--- flight.py
+++ flight.py
@@ -1,10 +1,6 @@
-
 import requests
 import json
 from PIL import Image, ImageDraw
-
-###MATH###
-rad_earth = 6371 #radius earth km
 
 r = requests.get('http://api.open-notify.org/iss-now.json')
 
@@ -37,25 +33,6 @@ image_width, image_height = bg.size
 
 x = int((image_width/360) * (180 + float(longitude)))
 y = int((image_height/180) * (90 - float(latitude)))
-
-"""
-
-
-def get_cartesian(lat=None,lon=None):
-    lat, lon = np.deg2rad(lat), np.deg2rad(lon)
-    x = rad_earth * np.cos(lat) * np.cos(lon)
-    y = rad_earth * np.cos(lat) * np.sin(lon)
-    return x,y
-
-
-xy = get_cartesian(float(latitude),float(longitude))
-
-print(xy)
-x = int(((xy[0])+6371)/(rad_earth/image_width))
-y = int(((xy[1])+6371)/(rad_earth/image_height))
-print(x,y)
-
-"""
 
 
 ##ISS ICON INITIALIZATION## 3444228.8365710177
